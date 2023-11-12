6c009208fa09b54c59c639a1a3d024328767d964
Donghun Won <wonhun1225@gmail.com>
2023-11-12 23:11:05 +0900

    test: orderTest 추가
    메뉴 합 테스트

---
+++ src/test/java/christmas/OrderTest.java
@@ -0,0 +1,22 @@
+package christmas;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import christmas.model.Order;
+import java.util.HashMap;
+import org.junit.jupiter.api.Test;
+
+public class OrderTest {
+    @Test
+    void orderSum() {
+        HashMap<String, String> input = new HashMap<String, String>();
+        input.put("타파스", "1");
+        input.put("제로콜라", "2");
+        Order menu = new Order(input);
+
+        int sum = menu.getSum();
+
+        assertEquals(sum, 11500);
+
+    }
+}
