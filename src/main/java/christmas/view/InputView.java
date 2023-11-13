46858898e35963818c44e6ec3d0c075a5b940429
Donghun Won <wonhun1225@gmail.com>
2023-11-13 20:07:06 +0900

    feat(view) : inputView 예외처리
    날짜 숫짜인지 확인
    날짜 1~31사이인지 확인
    메뉴 예시에 맞는지 확인
    메뉴 메뉴판에 있는지 확인
    메뉴 수량이 맞게 입력되었는지 확인
    메뉴가 중복되지는 않았는지 확인
    음료만 입력된 건 아닌지 확인

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,28 +1,94 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.model.Menu;
+import christmas.model.MenuGroup;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
 
 public class InputView {
     public String readDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         String input = Console.readLine();
-//        try {
-//            isCorrectMoney(money);
-//        } catch (IllegalArgumentException error) {
-//            System.out.println("[ERROR] 구입금액은 1,000원 단위로 입력해주세요.");
-//            moneyInput();
-//        }
+        try {
+            int convertInput = Integer.parseInt(input);
+            isInRange(convertInput);
+        } catch (IllegalArgumentException error) {
+            System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+            readDate();
+        }
         return input;
     }
-    public String[] readMenu(){
+
+    private void isInRange(int input) {
+        if (input < 1 || input > 31) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public String[] readMenu() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
-        String[] input = Console.readLine().split("[,\\-]");
-//        try {
-//            isCorrectMoney(money);
-//        } catch (IllegalArgumentException error) {
-//            System.out.println("[ERROR] 구입금액은 1,000원 단위로 입력해주세요.");
-//            moneyInput();
-//        }
+        String[] input = null;
+        try {
+            input = Console.readLine().split("[,\\-]");
+            menuCheck(input);
+            countCheck(input);
+            menuDupCheck(input);
+            checkOnlyDrink(input);
+        } catch (IllegalArgumentException error) {
+            System.out.println("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            readMenu();
+        }
         return input;
+    }
+
+    private void menuCheck(String[] input) {
+        for (int i = 0; i < input.length; i += 2) {
+            int finalI = i;
+            boolean isName = Arrays.stream(Menu.values()).anyMatch(v -> v.getName().equals(input[finalI]));
+            if (!isName) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private void countCheck(String[] input) {
+        int totalSum = 0;
+        for (int i = 1; i < input.length; i += 2) {
+            int convertInput = Integer.parseInt(input[i]);
+            totalSum += convertInput;
+            if (convertInput < 1) {
+                throw new IllegalArgumentException();
+            }
+            if (totalSum > 20) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private void menuDupCheck(String[] input) {
+        HashSet<String> setInput = new HashSet<>(Arrays.asList(input));
+
+        if (input.length != setInput.size()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkOnlyDrink(String[] input) {
+        List<Menu> menuList = MenuGroup.DRINK.getMenuList();
+        int count = 0;
+        for (int i = 0; i < input.length; i += 2) {
+            int finalI = i;
+            boolean match = menuList.stream()
+                    .anyMatch(menu -> Objects.equals(menu.getName(), input[finalI]));
+            if (match) {
+                count += 1;
+            }
+        }
+        if (count == input.length / 2) {
+            throw new IllegalArgumentException();
+        }
     }
 }
