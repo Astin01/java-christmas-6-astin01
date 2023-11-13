dd08767d79afd04648d2063b4b7fd5f96d782b7d
Donghun Won <wonhun1225@gmail.com>
2023-11-13 22:30:33 +0900

    refactor(model) : order 클래스 메서드 개선
    중복되는 메뉴 분류 로직을 sortMenu로 합쳐줌

--- src/main/java/christmas/model/Order.java
+++ src/main/java/christmas/model/Order.java
@@ -2,8 +2,10 @@ package christmas.model;
 
 import static java.lang.Integer.parseInt;
 
+import java.util.Arrays;
 import java.util.EnumMap;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
@@ -27,88 +29,25 @@ public class Order {
         this.orderSum = sum;
     }
 
-    public int getSum() {
-        return this.orderSum;
+    private void putMenu(String menu, String count) {
+        Arrays.stream(MenuGroup.values())
+                .forEach(menuGroup -> sortMenu(menu, count, menuGroup.getMenuList()));
     }
 
-    private void putMenu(String menu, String count) {
-        if (isMainDish(menu, count)) {
-            return;
+    private void sortMenu(String menu, String count, List<Menu> menuList) {
+        for (Menu targetMenu : menuList) {
+            if (targetMenu.getName().equals(menu)) {
+                order.put(targetMenu, count);
+            }
         }
-        if (isAppetizer(menu, count)) {
-            return;
-        }
-        if (isDrink(menu, count)) {
-            return;
-        }
-        isDessert(menu, count);
     }
 
     public Map<Menu, String> getOrder() {
         return this.order;
     }
 
-    private Boolean isDessert(String menu, String count) {
-        if (Objects.equals(menu, "초코케이크")) {
-            order.put(Menu.CHOCOLATE_CAKE, count);
-            return true;
-        }
-        if (Objects.equals(menu, "아이스크림")) {
-            order.put(Menu.CHOCOLATE_CAKE, count);
-            return true;
-        }
-        return false;
+    public int getSum() {
+        return this.orderSum;
     }
 
-    private Boolean isDrink(String menu, String count) {
-        if (Objects.equals(menu, "제로콜라")) {
-            order.put(Menu.ZERO_COLA, count);
-            return true;
-        }
-        if (Objects.equals(menu, "레드와인")) {
-            order.put(Menu.RED_WINE, count);
-            return true;
-        }
-        if (Objects.equals(menu, "샴페인")) {
-            order.put(Menu.CHAMPAGNE, count);
-            return true;
-        }
-        return false;
-    }
-
-    private Boolean isAppetizer(String menu, String count) {
-        if (Objects.equals(menu, "양송이수프")) {
-            order.put(Menu.MUSHROOM_SOUP, count);
-            return true;
-        }
-        if (Objects.equals(menu, "타파스")) {
-            order.put(Menu.TAPAS, count);
-            return true;
-        }
-        if (Objects.equals(menu, "시저샐러드")) {
-            order.put(Menu.CAESAR_SALAD, count);
-            return true;
-        }
-        return false;
-    }
-
-    private Boolean isMainDish(String menu, String count) {
-        if (Objects.equals(menu, "티본스테이크")) {
-            order.put(Menu.T_BONE_STEAK, count);
-            return true;
-        }
-        if (Objects.equals(menu, "바비큐립")) {
-            order.put(Menu.BARBECUE_LIB, count);
-            return true;
-        }
-        if (Objects.equals(menu, "해산물파스타")) {
-            order.put(Menu.SEAFOOD_PASTA, count);
-            return true;
-        }
-        if (Objects.equals(menu, "크리스마스파스타")) {
-            order.put(Menu.CHRISTMAS_PASTA, count);
-            return true;
-        }
-        return false;
-    }
 }
