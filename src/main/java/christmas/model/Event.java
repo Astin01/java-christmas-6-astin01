a0f20c3d652256a6f38cc2b4407210adecd67f2b
Donghun Won <wonhun1225@gmail.com>
2023-11-14 21:54:50 +0900

    feat(model) : event 클래스에 name 필드 추가

--- src/main/java/christmas/model/Event.java
+++ src/main/java/christmas/model/Event.java
@@ -1,20 +1,26 @@
 package christmas.model;
 
 public enum Event {
-    D_DAY(1000),
-    WEEKDAY(2023),
-    WEEKEND(2023),
-    SPECIAL(1000),
-    GIVE(25_000);
+    D_DAY(1000,"크리스마스 디데이 할인"),
+    WEEKDAY(2023,"평일 할인"),
+    WEEKEND(2023,"주말 할인"),
+    SPECIAL(1000,"특별 할인"),
+    GIVE(25_000,"증정 이벤트"),
+    NONE(0,"없음");
 
     final private int benefit;
+    final private String name;
 
-    Event(int benefit) {
+    Event(int benefit,String name) {
         this.benefit = benefit;
+        this.name = name;
     }
 
     public int getBenefit() {
         return benefit;
+    }
+    public String getName() {
+        return name;
     }
 
 
